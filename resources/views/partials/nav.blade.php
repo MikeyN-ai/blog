1377ff93d94f8479543e241145441c5fc780205f
MikeyN-ai <migcabgar@alu.edu.gva.es>
2026-02-03 17:59:00 +0100

    Añadir mi nombre al navbar

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -3,4 +3,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="{{ route('inicio')}}">Inicio</a>
     <a class="navbar-brand" href="{{ route('posts.index')}}">Listado de posts</a>
+    <span class="navbar-text">Miguel</span>
 </nav>
